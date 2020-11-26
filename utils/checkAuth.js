bc2fe6859870bc76608d68e3ce4755b7461c3e74
Kenneth <kyleabs20@gmail.com>
2020-11-25 17:29:17 -0800

    almost done with auth headers

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,1 +1,22 @@
+const { AuthenticationError } = require('apollo-server');
 const jwt = require('jsonwebtoken')
+const {SECRET_KEY} = require('../config')
+
+module.exports = (context)=>{
+        //getting the header from the global req
+        const authHeader = context.req.header.authorization;
+        if (!authHeader) throw new Error('Auth token must be provided'); 
+
+        // spliting the auth bearer from the token 
+        const token = authHeader.split('Bearer ')[1];
+        if (!token) throw new Error ('Auth token must be \'Bearer [token\' ')
+
+        try {
+            // getting the token 
+            const user = jwt.verify(token, SECRET_KEY)
+            return user;
+        } catch (error) {
+            throw new AuthenticationError("invalid token")
+        }
+
+}
