fbcc1d7c57c5ff72c585974e8b909868abf2be64
Kenneth <kyleabs20@gmail.com>
2020-11-25 16:06:18 -0800

    login almost done

--- resolvers/login.js
+++ resolvers/login.js
@@ -1,11 +1,33 @@
 const User = require('../models/registerModel.js');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {UserInputError} = require('apollo-server')
 
+const {SECRET_KEY} = require('../config')
 
 module.exports = {
     Mutation: {
         login: async(parent, {loginInput: {email, password}})=>{
-            
+            // making sure all fields are not empty
+            // checking if the email already exists
+            const emailCheck = await User.findOne({email}); 
+            if (!emailCheck) throw new UserInputError('email doesnot exist in the db', {
+                error: "user doesnot exist"
+            })
+
+            // decrypting the passsword
+            const passwordVerify = await bcrypt.compare(password, emailCheck.password); 
+            if (!passwordVerify) throw new UserInputError('password is not in the database', {
+                error: "incorrect password"
+            })
+
+            // providing a token for login
+            const token = jwt.sign({
+                id: emailCheck._id, 
+                email: emailCheck.email
+            }, SECRET_KEY, {expiresIn: "1h"})
+
+
         }
     }
 }
