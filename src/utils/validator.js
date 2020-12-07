76c9aeedf3ff855546a113d75d894003a13c02fd
Kenneth <kyleabs20@gmail.com>
2020-12-06 18:04:26 -0800

    hans zimmer

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -14,5 +14,23 @@ module.exports = {
         if (lastname.trim() = '') {
             errors.lastname = "lastname must not be empty"
         }
+        if (username.trim() = '') {
+            errors.username = "username must not be empty"
+        }
+        if (password.trim() = '') {
+            errors.password = "password field must not be empty"
+        } else {
+            const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+            if (!password.match(passwordRegex)) {
+                errors.password = "password field must contain at least an uppercase, a symbol and alphnumerica values"
+            } else if (password !== confirmPassword) {
+                errors.password = "passwords must match"
+            }
+        }
+
+        return {
+            errors, 
+            valid: Object.keys(errors).length < 1
+        }
     }
-}``
+}
