194703a358d4e39f6573f05b0b327636a97f812c
Kenneth <kyleabs20@gmail.com>
2020-12-09 12:40:50 -0800

    pushing to heroku

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 
 dotenv.config()
 
+const PORT = process.env.PORT || 5000
+
 // imports 
 const { resolvers } = require('./src/graphql/resolvers/index')
 const typeDefs = require('./src/graphql/typeDefs')
@@ -19,7 +21,7 @@ const server = new ApolloServer(
 mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser: true, useUnifiedTopology: true})
         .then(()=>{
             console.log("Database connection established successfuly")
-            return server.listen({port: process.env.PORT || 5000 })
+            return server.listen({port: PORT})
         })
         .then(({url})=> console.log(`server connection established successfully on url ${url}`))
 
