3fa325a999e8c498f9f6c3b3c85146997ce5a23e
Kenneth <kyleabs20@gmail.com>
2020-12-11 19:38:21 -0800

    made it this far

---
+++ src/graphql/resolvers/admin.js
@@ -0,0 +1,11 @@
+const  MainAdmin = require('../../models/AdminModel'); 
+const SecAdmin = require('../../models/secondaryAdminModel'); 
+
+
+module.exports = {
+    Mutation = {
+        addMainAdmin: async (parent, {input: {}})=>{
+
+        }
+    }
+}
--- src/utils/validator.js
+++ src/utils/validator.js
@@ -55,6 +55,10 @@ const LoginValidation = ()=>{
         }
 }
 
+const MainAdminValidation = ()=>{
+    
+}
+
 
 module.exports = {
    LoginValidation, 
