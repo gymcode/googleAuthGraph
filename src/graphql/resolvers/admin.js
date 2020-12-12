60d8314ecebbe173e226ce771f768f28c7a57815
Kenneth <kyleabs20@gmail.com>
2020-12-11 20:35:54 -0800

    guns to our fist

--- src/graphql/resolvers/admin.js
+++ src/graphql/resolvers/admin.js
@@ -1,11 +1,17 @@
 const  MainAdmin = require('../../models/AdminModel'); 
 const SecAdmin = require('../../models/secondaryAdminModel'); 
+const {MainAdminValidation} = require('../../utils/validator')
+const {UserInputError} = require('apollo-server')
 
 
 module.exports = {
     Mutation: {
         addMainAdmin: async (_, {input: {firstname, othernames, email, password}})=>{
             // TODO input validation
+            const {errors, valid} = MainAdminValidation(firstname, othernames, email, password)
+            if (!valid) {
+                throw new UserInputError('error', {errors})
+            }
             // TODO password hashing 
             // TODO check if there is a user in it already 
             // TODO if yes then cannot add onother super user
