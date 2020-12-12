7ce5f02e06f494fc8ec757b2696c1ca96bd593ff
Kenneth <kyleabs20@gmail.com>
2020-12-11 23:26:02 -0800

    sleepy

--- src/graphql/resolvers/admin.js
+++ src/graphql/resolvers/admin.js
@@ -1,8 +1,8 @@
-const  MainAdmin = require('../../models/AdminModel'); 
+const  {MainAdminModel} = require('../../models/AdminModel'); 
 const SecAdmin = require('../../models/secondaryAdminModel'); 
 const {MainAdminValidation} = require('../../utils/validator')
 const {UserInputError} = require('apollo-server')
-
+const bcrypt = require('bcryptjs')
 
 module.exports = {
     Mutation: {
@@ -12,8 +12,17 @@ module.exports = {
             if (!valid) {
                 throw new UserInputError('error', {errors})
             }
-            // TODO password hashing 
             // TODO check if there is a user in it already 
+            const userCheck = await MainAdminModel.find({email})
+            if (userCheck) {
+                return {
+                    status: "User already exist no more can be added"
+                }
+            }
+            // TODO password hashing 
+            const SaltGen = await bcrypt.genSalt(12); 
+            const hashedPassword = await bcrypt.hash(password, SaltGen)
+
             // TODO if yes then cannot add onother super user
         }
     }
