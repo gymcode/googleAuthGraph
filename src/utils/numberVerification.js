787cb46b972c366145a7eb4adec22858160e4628
Kenneth <kyleabs20@gmail.com>
2020-12-15 20:27:13 -0800

    this is a commit message

--- src/utils/numberVerification.js
+++ src/utils/numberVerification.js
@@ -1,4 +1,5 @@
 var client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN)
+const {AuthenticationError} = require('apollo-server')
 
 module.exports = {
     sendVerification: (phone)=>{
@@ -7,7 +8,20 @@ module.exports = {
             .create({to: phone, channel: process.env.CHANNEL})
     }, 
 
-    checkToken: (phone)=>{
-
-    }
+    checkToken: (phone, code, context)=>{
+        //getting the header from the global req
+        const adminCode = context.req.headers.authorization; 
+        if (admincode) {
+           try {
+               const code =  client.verify.services(process.env.ACCOUNT_SID)
+                            .verificationChecks
+                            .create({to: phone, code: code})
+                            .then(veri => console.log(veri.status))
+                return code;
+           } catch (error) {
+               throw new AuthenticationError("invalid code")
+           }
+        } 
+        throw new Error("must make sure a verification code has been entered")
+    }   
 }
