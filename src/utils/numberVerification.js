5f9341a642a29c87b93d4d86e0ac56ed48e724d8
Kenneth <kyleabs20@gmail.com>
2020-12-15 20:09:51 -0800

    you care

--- src/utils/numberVerification.js
+++ src/utils/numberVerification.js
@@ -2,11 +2,12 @@ var client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN)
 
 module.exports = {
     sendVerification: (phone)=>{
-         client.verify.services(process.env.ACCOUNT_SID)
+         return client.verify.services(process.env.ACCOUNT_SID)
             .verifications
             .create({to: phone, channel: process.env.CHANNEL})
-            .then(verification => {
-                return verification.status
-            })
+    }, 
+
+    checkToken: (phone)=>{
+
     }
 }
